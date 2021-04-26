e05d895ad53beeba859b2ec95ed8003e3f699da3
Larry Jackson <60943960+LarryProgrammer@users.noreply.github.com>
2021-04-26 14:06:53 -0400

    Update HighestScore.java

--- FileOperations/src/main/java/FileOperations/HighestScore.java
+++ FileOperations/src/main/java/FileOperations/HighestScore.java
@@ -16,7 +16,9 @@ import java.util.*;
 public class HighestScore {
     public static void main(String[] args) {
         try {
+            /* Reading each line of gamescore file using Scanner class */
             Scanner file = new Scanner(new File("gamescore.txt"));
+             /* determine the high score */
             int highest = file.nextInt();
 
             while (file.hasNext()) {
@@ -25,12 +27,14 @@ public class HighestScore {
                 if(score > highest) {
                     highest = score;
                 }
+                /* Display all the scores first */
                 System.out.println(score);
             }
             file.close();
-
+            /* display the high score */
             System.out.println("The highest score in the file is: " + highest);
         }
+        /* display a message if the text file couldn't be found */
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
